36c47c9372d30dfcab8ca61559d162a8dc02e52b
Rajesh@2003 <rajesh7708k@gmail.com>
2026-07-04 12:26:09 +0530

    Implement Client module backend: UI roles, DB schema, Requests, and Controller

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -6,8 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
+    protected $guarded = [];
+
     protected $casts = [
         'pan_number' => 'encrypted',
         'gstin' => 'encrypted',
     ];
+
+    public function contacts()
+    {
+        return $this->hasMany(ClientContact::class);
+    }
+
+    public function branches()
+    {
+        return $this->hasMany(ClientBranch::class);
+    }
+
+    public function documents()
+    {
+        return $this->hasMany(ClientDocument::class);
+    }
+
+    public function accountManager()
+    {
+        return $this->belongsTo(User::class, 'account_manager_id');
+    }
 }
--- app/Models/ClientBranch.php
+++ app/Models/ClientBranch.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class ClientContact extends Model
+class ClientBranch extends Model
 {
     protected $guarded = [];
 }
